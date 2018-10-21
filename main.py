35d320a5d9b6970f5c2c1b9048ac0ab305fb6403
Yavuz Burak Saklı <yburaksakli@hotmail.com>
2018-10-21 21:12:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,21 +20,37 @@ class App(QMainWindow):
         self.inputLoaded = False
         self.targetLoaded = False
         self.title = 'Histogram Equalization'
+        self.input_image_path = ""
         self.input_image = QLabel()
-        self.input_histogram = QLabel()
+        self.input_histogram = FigureCanvas(Figure(figsize=(5, 3)))
+        self.target_image_path = ""
         self.target_image = QLabel()
-        self.target_histogram = QLabel()
+        self.target_histogram = FigureCanvas(Figure(figsize=(5, 3)))
         self.result = QLabel()
         self.result_histogram = QLabel()
         self.initUI()
 
-    def openInputImage(self):
+    def open_input_image(self):
         # This function is called when the user clicks File->Input Image.
-        return NotImplementedError
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.input_image_path, _ = QFileDialog.getOpenFileName(self, "Select Input Image", "",
+                                                               "PNG Files (*.png)", options=options)
+        if self.input_image_path:
+            image = QImage(self.input_image_path)
+            self.input_image.setPixmap(QPixmap.fromImage(image))
+            self.inputLoaded = True
 
-    def openTargetImage(self):
+    def open_target_image(self):
         # This function is called when the user clicks File->Target Image.
-        return NotImplementedError
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.target_image_path, _ = QFileDialog.getOpenFileName(self, "Select Target Image", "",
+                                                                "PNG Files (*.png)", options=options)
+        if self.target_image_path:
+            image = QImage(self.target_image_path)
+            self.target_image.setPixmap(QPixmap.fromImage(image))
+            self.targetLoaded = True
 
     def initUI(self):
         # Menu Actions
@@ -43,13 +59,13 @@ class App(QMainWindow):
         open_input_action = QAction('&Open Input', self)
         open_input_action.setShortcut('Ctrl+O')
         open_input_action.setStatusTip('Open the Input Image')
-        open_input_action.triggered.connect(self.openInputImage)
+        open_input_action.triggered.connect(self.open_input_image)
 
         # Opening the target image
         open_target_action = QAction('&Open Target', self)
         open_target_action.setShortcut('Ctrl+T')
         open_target_action.setStatusTip('Open the Target Image')
-        open_target_action.triggered.connect(self.openTargetImage)
+        open_target_action.triggered.connect(self.open_target_image)
 
         # Exiting the program from menu
         exit_act = QAction('&Exit', self)
@@ -63,25 +79,25 @@ class App(QMainWindow):
         file_menu.addAction(open_target_action)
         file_menu.addAction(exit_act)
 
-        # Histogram button
+        # 'Calculate Histogram' button
+        calculate_histogram = QAction('Calculate Histograms', self)
+        calculate_histogram.setShortcut('Ctrl+C')
+        calculate_histogram.triggered.connect(self.calculateHistogramClicked)
+
+        # 'Equalize Histogram' button
         equalize_histogram = QAction('Equalize Histogram', self)
         equalize_histogram.setShortcut('Ctrl+H')
         equalize_histogram.triggered.connect(self.histogramButtonClicked)
 
         toolbar = self.addToolBar('Equalize Histogram')
+        toolbar.addAction(calculate_histogram)
         toolbar.addAction(equalize_histogram)
 
-        image1 = QImage("color1.png")
-        image2 = QImage("color2.png")
-
+        # Grid elements
         self.input_image = QLabel()
-        self.input_histogram = QLabel()
-        self.input_histogram.setText("Input Histogram Placeholder")
-        self.input_image.setPixmap(QPixmap.fromImage(image1))
+        self.input_image.setText("Input Image Placeholder")
         self.target_image = QLabel()
-        self.target_histogram = QLabel()
-        self.target_image.setPixmap(QPixmap.fromImage(image2))
-        self.target_histogram.setText("Target Histogram Placeholder")
+        self.target_image.setText("Target Image Placeholder")
         self.result = QLabel()
         self.result_histogram = QLabel()
 
@@ -96,7 +112,6 @@ class App(QMainWindow):
 
         grid.addWidget(self.result, 0, 2)
         grid.addWidget(self.result_histogram, 1, 2)
-        self.result_histogram.setText("Asasasa")
         central_widget = QWidget()
         central_widget.setLayout(grid)
         self.setCentralWidget(central_widget)
@@ -105,31 +120,71 @@ class App(QMainWindow):
         self.showMaximized()
         self.show()
 
-    def histogramButtonClicked(self):
+    def calculateHistogramClicked(self):
         if not self.inputLoaded and not self.targetLoaded:
             # Error: "First load input and target images" in MessageBox
+            self.statusBar().showMessage("Error: First load input and target images")
             return NotImplementedError
         if not self.inputLoaded:
             # Error: "Load input image" in MessageBox
+            self.statusBar().showMessage("Error: Load input image first")
+            hist2 = self.calc_histogram(self.target_image_path)
+            self.input_histogram = PlotCanvas(hist2, self)
             return NotImplementedError
         elif not self.targetLoaded:
             # Error: "Load target image" in MessageBox
+            self.statusBar().showMessage("Error: Load target image first")
+            hist1 = self.calc_histogram(self.input_image_path)
+            self.input_histogram = PlotCanvas(hist1, self)
+            return NotImplementedError
+        # Calculate and display both images' histograms
+        hist1 = self.calc_histogram(self.input_image_path)
+        self.input_histogram = PlotCanvas(hist1, self)
+        hist2 = self.calc_histogram(self.target_image_path)
+        self.input_histogram = PlotCanvas(hist2, self)
+
+    def histogramButtonClicked(self):
+        if not self.inputLoaded and not self.targetLoaded:
+            # Error: "First load input and target images" in MessageBox
+            self.statusBar().showMessage("Error: First load input and target images")
+            return NotImplementedError
+        if not self.inputLoaded:
+            # Error: "Load input image" in MessageBox
+            self.statusBar().showMessage("Error: Load input image first")
+            return NotImplementedError
+        elif not self.targetLoaded:
+            # Error: "Load target image" in MessageBox
+            self.statusBar().showMessage("Error: Load target image first")
             return NotImplementedError
 
-    def calcHistogram(self, I):
+    def calc_histogram(self, image):
         # Calculate histogram
-        return NotImplementedError
-
+        img = cv2.imread(image, 1)
+        R, C, B = img.shape
+        hist = np.zeros([256, 1, B], dtype=np.uint8)
+        for g in range(256):
+            hist[g, 0, ...] = np.sum(np.sum(img == g, 0), 0)
+        return hist
 
 class PlotCanvas(FigureCanvas):
-    def __init__(self, hist, parent=None, width=5, height=4, dpi=100):
-        # Init Canvas
-        self.plotHistogram(hist)
+    def __init__(self, hist, parent, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
 
-    def plotHistogram(self, hist):
-        return NotImplementedError
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot_histogram(hist)
+
+    def plot_histogram(self, hist):
         # Plot histogram
-
+        ax = self.figure.add_subplot(111)
+        ax.plot(hist, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
         self.draw()
 
 
